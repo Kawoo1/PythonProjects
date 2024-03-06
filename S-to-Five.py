f6613791d17b4698d24ee1a84255fc07b9ba592b
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2024-03-05 20:52:16 -0700

    03-05-2024 Instructions Update S-to-Five.py
    Created a comment section with instructions explaining how to use the python script.

--- S-to-Five.py
+++ S-to-Five.py
@@ -1,11 +1,18 @@
 # S-to-Five takes a word or pdf document and converts every "s" character to a 5.
+# This file was authored by: Kyle Shanahan
 
 # These libraries need to be installed for the script to work:
 # pip install python-docx
 # pip install PyPDF2
 
-# 
-#
+# Steps to run the script:
+# 1) Save the Script: Save the provided Python script to a file with a .py extension. You can name it whatever you like, such as text_replace.py.
+# 2) Navigate to Script Directory: Open a terminal or command prompt and navigate to the directory where you saved the Python script using the cd command:cd path/to/directory
+# 3) Execute the script:
+# For Word Documents --> python text_replace.py your_document.docx <-- Make sure to replace "your_document" with the document name of your file
+# For PDF Documents --> python text_replace.py your_document.pdf <-- Make sure to replace "your_document" with the document name of your file
+# After running the script, it will create a modified version of your input document with 's' replaced by '5'. If it's a PDF file, the modified file will have a name starting with 'modified_', followed by the original file name.
+
 
 import os
 import sys
